24ded0f8ffe7148e03c220c36b565f814df55c5e
Schenier Lpez <schnierlopez@gmail.com>
2021-11-27 20:42:27 +0100

    aaaw

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ config();
                                     host: string;
                                     user?: string;
                                     pass?: string;
-                                }
+                        }
 
                 export default <iConfig>{
                     host: 'http://' + process.env.ELASTIC_HOST as string + ":" + process.env.ELASTIC_PORT
